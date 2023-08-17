d11d33490ea9d5bcce3017cc70fa860809cbd035
StellaA30 <56633439+StellaA30@users.noreply.github.com>
2023-08-17 20:59:51 +0100

    adds code for File writing

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -1,8 +1,7 @@
 package com.cbfacademy;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
@@ -18,16 +17,74 @@ public class App {
                     String outputLine = lineReader.readLine();
                     while (outputLine != null ){
 
-                        // if statement to skip empty lines
-                        if(!outputLine.isEmpty()){
-                            System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
-                        }
-//                        System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
+//                        // if statement to skip empty lines
+//                        if(!outputLine.isEmpty()){
+//                            System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
+//                        }
+                        System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
                         outputLine = lineReader.readLine();
                     }
         } catch (IOException e){
             System.out.println("File reading exception: " + e.getMessage());
         }
+
+
+//        String invertFile = "/Users/stellaannor/Documents/Coding_Black_Females/java-exercises-StellaA30/exercises/input-output/src/main/resources/inverted.txt";
+
+//        try (
+//                FileOutputStream fileOutputStream = new FileOutputStream(invertFile);
+//                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+//                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+//        )
+
+//
+
+        String inputFilePath = "/Users/stellaannor/Documents/Coding_Black_Females/java-exercises-StellaA30/exercises/input-output/src/main/resources/exercise.txt";
+        String invertFilePath = "/Users/stellaannor/Documents/Coding_Black_Females/java-exercises-StellaA30/exercises/input-output/src/main/resources/inverted.txt";
+
+        try (
+                FileInputStream fileInputStream = new FileInputStream(inputFilePath);
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                FileOutputStream fileOutputStream = new FileOutputStream(invertFilePath);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        ) {
+
+            // create empty list of strings
+            List<String> lines = new ArrayList<>();
+            String line;
+
+            // Read the content of the exercise.txt file
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Write the inverted content to the inverted.txt file
+            for (int i = lines.size() - 1; i >= 0; i--) {
+                bufferedWriter.write(lines.get(i));
+                bufferedWriter.newLine();
+            }
+
+            System.out.println("Content inverted and written to " + invertFilePath);
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
